a8bc2f867f1aa394e0670bc0e500e443b6d9b199
YoYoDough <yosuf818s@gmail.com>
2022-05-24 02:57:58 -0700

    Let the User know the final Score

--- code.js
+++ code.js
@@ -10,42 +10,42 @@ function playRound(playerSelectionn, computerSelectionn)
     var computerSelection = computerSelectionn.toLowerCase();
     if (computerSelection == playerSelection)
     {
-        var draw = "Draw";
+        var draw = "Draw, you both picked the same";
         return draw;
     }
     if (computerSelection == "paper" && playerSelection == "rock")
     {
-        var lost = "You lose, Paper beats Rock.";
+        var lost = "You lose, Paper beats Rock and Computer picked Paper.";
         computerSelectionCount++;
         return lost;
     }
     else if (computerSelection == "rock" && playerSelection == "paper")
     {
-        var winner = "You win, Paper beats Rock."
+        var winner = "You win, Paper beats Rock. Computer picked Rock."
         playerSelectionCount++;
         return winner;
     }
     if (computerSelection == "scissors" && playerSelection == "paper")
     {
-        var loser = "You lose, Scissors beats Paper."
+        var loser = "You lose, Scissors beats Paper. Computer picked Scissors."
         computerSelectionCount++;
         return loser;
     }
     else if (computerSelection == "paper" && playerSelection == "scissors")
     {
-        var winner = "You win, Scissors beats Paper."
+        var winner = "You win, Scissors beats Paper. Computer picked Paper."
         playerSelectionCount++;
         return winner;
     }
     if (computerSelection == "scissors" && playerSelection == "rock")
     {
-        var winner = "You win, Rock beats Scissors."
+        var winner = "You win, Rock beats Scissors. Computer picked Scissors."
         playerSelectionCount++;
         return winner;
     }
     else if (computerSelection == "rock" && playerSelection == "scissors")
     {
-        var loser = "You lose, Rock beats Scissors."
+        var loser = "You lose, Rock beats Scissors. Computer picked Rock."
         computerSelectionCount++;
         return loser;
     }
@@ -62,4 +62,17 @@ for(i = 0; i < 5; i++)
     var computerSelectionn = computerPlay();
     console.log(game(playerSelectionn, computerSelectionn))
 }
+console.log("Player score To Computer Score");
 console.log(playerSelectionCount + " To " + computerSelectionCount);
+if (playerSelectionCount > computerSelectionCount)
+{
+    console.log("You win")
+}
+else if (playerSelectionCount < computerSelectionCount)
+{
+    console.log("You lose")
+}
+else if (playerSelectoinCount == computerSelectionCount)
+{
+    console.log("Its a Draw")
+}
