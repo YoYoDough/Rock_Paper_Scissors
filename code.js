7029a4ca49a6c5092909335301411e7f3cb5cae9
YoYoDough <yosuf818s@gmail.com>
2022-05-24 01:07:38 -0700

    Just finished the playRound function and was able to add the counter to see who won the round.

---
+++ code.js
@@ -0,0 +1,57 @@
+function computerPlay()
+{
+    var items = ['Rock', 'Paper', 'Scissors'];
+    var item = items[Math.floor(Math.random() * items.length)];
+    return item;
+}
+function playRound(playerSelection, computerSelection)
+{
+    if (computerSelection == playerSelection)
+    {
+        var draw = "Draw";
+        return draw;
+    }
+    if (computerSelection == "paper" && playerSelection == "rock")
+    {
+        var lost = "You lose, Paper beats Rock.";
+        computerSelectionCount++;
+        return lost;
+    }
+    else if (computerSelection == "rock" && playerSelection == "paper")
+    {
+        var winner = "You win, Paper beats Rock."
+        playerSelectionCount++;
+        return winner;
+    }
+    if (computerSelection == "scissors" && playerSelection == "paper")
+    {
+        var loser = "You lose, Scissors beats Paper."
+        computerSelectionCount++;
+        return loser;
+    }
+    else if (computerSelection == "paper" && playerSelection == "scissors")
+    {
+        var winner = "You win, Scissors beats Paper."
+        playerSelectionCount++;
+        return winner;
+    }
+    if (computerSelection == "scissors" && playerSelection == "rock")
+    {
+        var winner = "You win, Rock beats Scissors."
+        playerSelectionCount++;
+        return winner;
+    }
+    else if (computerSelection == "rock" && playerSelection == "scissors")
+    {
+        var loser = "You lose, Rock beats Scissors."
+        computerSelectionCount++;
+        return loser;
+    }
+}
+var playerSelectionCount = 0;
+var computerSelectionCount = 0;
+var playerSelectionn = "PAper";
+var computerSelectionn = computerPlay();
+var playerSelection = playerSelectionn.toLowerCase();
+var computerSelection = computerSelectionn.toLowerCase();
+console.log(playRound(playerSelection, computerSelection));
